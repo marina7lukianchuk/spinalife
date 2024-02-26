51c9c48cef55e027c46eb4922d312a7bddf48446
Artem <artemtruskiyweb@gmail.com>
2024-02-26 10:52:20 +0200

    added block with specialists

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,8 +10,8 @@
             <p class="special_offer">СПЕЦІАЛЬНА ПРОПОЗИЦІЯ!</p>
             <p class="special_offer--text">
               Запишись на консультацію зараз і отримай 500 грн на свій депозит
-              <span class="special_offer--small">При купівлі абонемента в день консультації — +1 заняття в
-                подарунок</span>
+              <p class="special_offer--small">При купівлі абонемента в день консультації — +1 заняття в
+                подарунок</p>
             </p>
           </div>
           <form class="form" @submit.prevent="submitForm">
